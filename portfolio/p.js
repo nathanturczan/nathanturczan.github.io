2d12b3c7e5f02a09f397c02e0b600f3b1b5cd9c5
nathanturczan <nathanturczan@gmail.com>
2020-04-06 13:50:05 -0700

    finishing interstices and updating portfolio

--- portfolio/p.js
+++ portfolio/p.js
@@ -6,9 +6,9 @@ function getPortfolio()
         "title": "The Scale Navigator",
         "img": "scale_nav_smaller.gif",
         "date": "2018-present (in development)",
-        "format": "for improvising ensemble",
+        "format": "a web application for exploring harmony",
         "url": "https://www.nathanturczan.com/apps/",
-        "desc": "<a href=\"scale_nav_explanation/index.html\">explanation</a>"
+        "desc": "read an <a href=\"scale_nav_explanation/index.html\">explanation</a>"
     },
     {
         "title": "Modal Intersections // Thought Loops",
@@ -16,23 +16,15 @@ function getPortfolio()
         "date": "2018 - 2019",
         "format": "for CalArts WaveCave",
         "url": "https://www.nathanturczan.com/installation/",
-        "desc": "<a href=\"modal_intersections_explanation/index.html\">explanation</a>"
+        "desc": "read an <a href=\"modal_intersections_explanation/index.html\">explanation</a>"
     },
     {
-        "title": "INTERSTICES // Heptagrams",
-        "img": "hep1.gif",
+        "title": "Networked Music with the Scale Navigator",
+        "img": "inter.gif",
         "date": "2018 - 2019",
-        "format": "for improvising ensemble",
-        "url": "https://www.chordascollective.org/heptagrams-1",
-        "desc": "<a href=\"INTERSTICES_explanation/index.html\">explanation</a>"
-    },
-    {
-        "title": "The Just Man",
-        "img": "jm.gif",
-        "date": "2019 - 2020",
-        "format": "for Chordas Collective",
-        "url": "https://github.com/nathanturczan/The-Just-Man",
-        "desc": "<a href=\"just_man_explanation/index.html\">explanation</a>"
+        "format": "for CalArts Machine Orchestra",
+        "url": "https://youtu.be/w1Bs_j3KwBY",
+        "desc": "read an <a href=\"INTERSTICES_explanation/index.html\">explanation</a>"
     }
     ];
 }
