861a8874cf6f048ffdacbbc6467885008caa4cdc
Seraphina Oney <soney@RCSs-MacBook-Pro-9.local>
2020-07-04 11:10:13 -0700

    added flute notes

--- apps/fret/script.js
+++ apps/fret/script.js
@@ -57,18 +57,20 @@ function HideFluteSVG(svg_name) {
 
 function displayFluteDiagrams(scaleNotes) {
     var allImages = document.querySelectorAll('img');
-    allImages[0].src = 'trills/4-6.svg';
-    allImages[1].src = 'trills/5-6.svg';
-    allImages[2].src = 'trills/6-8.svg';
-    allImages[3].src = 'trills/9-10.svg';
-    allImages[4].src = 'trills/8-10.svg';
-    allImages[5].src = 'trills/10-11.svg';
-    allImages[6].src = 'trills/10-0.svg';
-    allImages[7].src = 'trills/11-1.svg';
-    allImages[8].src = 'trills/0-1.svg';
-    allImages[9].src = 'trills/0-2.svg';
-    allImages[10].src = 'trills/1-2.svg';
-    allImages[11].src = 'trills/1-3.svg';
+    allImages[0].src = 'trills/2-4.svg';
+    allImages[1].src = 'trills/3-5.svg';
+    allImages[2].src = 'trills/4-6.svg';
+    allImages[3].src = 'trills/5-6.svg';
+    allImages[4].src = 'trills/6-8.svg';
+    allImages[5].src = 'trills/8-9.svg';
+    allImages[6].src = 'trills/8-10.svg';
+    allImages[7].src = 'trills/9-10.svg';
+    allImages[8].src = 'trills/10-11.svg';
+    allImages[9].src = 'trills/10-0.svg';
+    allImages[10].src = 'trills/0-1.svg';
+    allImages[11].src = 'trills/0-2.svg';
+    allImages[12].src = 'trills/1-2.svg';
+    allImages[13].src = 'trills/1-3.svg';
 
 
     for(var i = 0; i < allImages.length ; i++) {
