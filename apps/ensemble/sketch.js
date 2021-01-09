7f547ed9d26e5a95212194277297ec8c501be340
Seraphina Oney <soney@RCSs-MacBook-Pro-9.local>
2021-01-08 17:24:34 -0800

    add mandolin fret marker at the 10th fret

--- apps/ensemble/sketch.js
+++ apps/ensemble/sketch.js
@@ -1,4 +1,38 @@
+//Detect mobile platform
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if ( isMobile.any() ) {
+
+    if (window.innerHeight > window.innerWidth) {
+       console.log("Orientation is in portrait");
+       }
+    else {
+       console.log("Orientation is in landscape");
+         }
+    }
+else {
+   console.log("Mobile platform not detected.");
+};
 
 
 
